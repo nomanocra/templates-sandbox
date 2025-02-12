724346f6dc1e925742970c7fd127432de8632c09
David Duprat <dprt.david@gmail.com>
2025-02-12 11:33:55 +0100

    Adding mobile responsive behavior with media query.

--- SIdeBar_vanilla/js/script.js
+++ SIdeBar_vanilla/js/script.js
@@ -4,9 +4,15 @@ const sidebarToggle = document.getElementById('sidebar-toggle');
 /* eslint-disable no-unused-vars */
 function toggleSubMenu(btn) {
   const subMenu = btn.nextElementSibling;
+
+  if (!subMenu.classList.contains('show')) {
+    closeAllSubMenus();
+  }
+
   subMenu.classList.toggle('show');
   btn.classList.toggle('rotate-icon');
 
+  //If we open a submenu when the sidebar is closed, we need to open the sidebar
   if (sidebar.classList.contains('close')) {
     sidebar.classList.toggle('close');
     sidebarToggle.classList.toggle('rotate-icon');
@@ -18,6 +24,10 @@ function toggleSidebar(btn) {
   btn.classList.toggle('rotate-icon');
 
   // Close all submenus when the sidebar is toggled
+  closeAllSubMenus();
+}
+
+function closeAllSubMenus() {
   Array.from(sidebar.getElementsByClassName('show')).forEach((element) => {
     element.previousElementSibling.classList.remove('rotate-icon');
     element.classList.remove('show');
